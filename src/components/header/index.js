29a0f20adadb595b54eb8a848690e0b133e85231
maudnals <maud.nalpas@gmail.com>
2020-03-16 16:44:20 +0100

    feat: added moar styles

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,22 +1,32 @@
-import { h } from 'preact';
-import { Link } from 'preact-router/match';
-import style from './style.css';
+import { h } from 'preact'
+import { Link } from 'preact-router/match'
+import style from './style.css'
+
+const routes = {
+  xss: {
+    label: 'XSS',
+    href: '/'
+  },
+  profile: {
+    label: 'profile',
+    href: '/profile'
+  },
+  john: {
+    label: 'profile/john',
+    href: '/profile/john'
+  }
+}
 
 const Header = () => (
-	<header class={style.header}>
-		--xpsd
-		<nav>
-			<Link activeClassName={style.active} href="/">
-				XSS
-			</Link>
-			<Link activeClassName={style.active} href="/profile">
-				Oh
-			</Link>
-			<Link activeClassName={style.active} href="/profile/john">
-				No
-			</Link>
-		</nav>
-	</header>
-);
+  <header class={style.header}>
+    <nav>
+      {Object.keys(routes).map(r =>
+        (<Link activeClassName={style.active} href={routes[r].href}>
+          {routes[r].label}
+        </Link>)
+      )}
+    </nav>
+  </header>
+)
 
-export default Header;
+export default Header
